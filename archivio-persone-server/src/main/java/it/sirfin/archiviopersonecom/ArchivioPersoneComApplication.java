697d1860fa2ac9e5aa72f409fba37f29ddefbd0e
ValeriaCom <78487304+ValeriaCom@users.noreply.github.com>
2021-03-07 15:59:05 +0100

    commit iniziale, applicazione vuota

---
+++ archivio-persone-server/src/main/java/it/sirfin/archiviopersonecom/ArchivioPersoneComApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.archiviopersonecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioPersoneComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioPersoneComApplication.class, args);
+	}
+
+}
---
+++ archivio-persone-server/src/test/java/it/sirfin/archiviopersonecom/ArchivioPersoneComApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.archiviopersonecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivioPersoneComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
